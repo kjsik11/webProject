dd771bf8bc5337a13930da68d7747402200c9cf1
kjsik11 <71022607+kjsik11@users.noreply.github.com>
2021-04-23 20:16:42 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,16 +29,16 @@ const Home: React.FC = () => {
           <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
             <div className="rounded-md shadow">
               <a
-                href="/information"
-                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                href="/team-project"
+                className="w-full flex items-center justify-center px-2 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
               >
-                자세한 설명 보러가기
+                팀 프로젝트 설명 보러가기
               </a>
             </div>
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
               <a
                 href="/profile"
-                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+                className="w-full flex items-center justify-center px-2 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
               >
                 프로필 보러가기
               </a>
