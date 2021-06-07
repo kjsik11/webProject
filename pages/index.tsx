9003dd0317ae85973bac0e0e78b94b2275bc5547
jongsik <kjsik11@gmail.com>
2021-06-07 20:50:59 +0900

    /

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: React.FC = () => {
       <main className="my-4 mx-auto max-w-7xl px-4 sm:my-16">
         <div className="text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">중간고사</span>
+            <span className="block xl:inline">개인</span>
             <span className="block text-indigo-600 xl:inline"> 홈페이지</span>
           </h1>
           <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
